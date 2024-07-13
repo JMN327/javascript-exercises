5273c3266ff0d41032fb609876a7c10c3998891b
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-13 21:39:05 +0100

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let text = "";
+    for (let index = 0; index < num; index++) {
+        
+        text += string;
+    }
+    return text;
 };
 
 // Do not edit below this line
