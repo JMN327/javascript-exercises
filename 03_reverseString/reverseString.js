7431f72a58785aff016de815898ba7e1e33cb10d
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-13 22:05:28 +0100

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array1 = string.split("");
+    let array2 = [];
+    while (array1.length > 0) {
+        array2.push(array1.pop());
+    }
+    let result = array2.join("");
+    return result
 };
 
 // Do not edit below this line
