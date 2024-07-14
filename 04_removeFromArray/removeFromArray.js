e341db099991bff86187f17c913d3acc1c5a428a
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-14 08:46:06 +0100

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    const array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        let check = array.indexOf(arguments[i]);
+        while (check !== -1) {
+            array.splice(check,1);
+            check = array.indexOf(arguments[i]);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
