27490bb51fee0e2d19755ffe9b33a6e0ace32eb0
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-14 09:11:01 +0100

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (a < 0 
+        || b < 0 
+        || typeof a !== "number" 
+        || typeof b !== "number"
+        || Array.isArray(a)
+        || Array.isArray(b)) {
+        return "ERROR"
+    }
+    return ((Math.max(a,b) - Math.min(a,b) + 1)/2) * (a + b);
 };
 
 // Do not edit below this line
