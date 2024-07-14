ce269530e8baf4fa65d63ad2f0ee15f3e23a1171
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-14 09:28:32 +0100

    Finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    if (typeof x !== "number" || Array.isArray(x)) {
+        return "ERROR"        
+    }
+    return ((x % 4 === 0) && ((x % 100 !== 0) || x % 400 === 0));
 };
 
 // Do not edit below this line
