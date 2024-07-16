0f35dc27d27ad7bcc4fea002321273edaa3aa820
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-16 23:15:15 +0100

    Add solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((a, b) => a + b, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	const product = array.reduce((a, b) => a * b, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+if (a === 0) {
+  return 1;
+}
+let product = a;
+for (let i = a - 1; i > 1; i--) {
+  product *= i ;
+}
+return product;
 };
 
 // Do not edit below this line
