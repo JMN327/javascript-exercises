39480dd65ea4eadaa160620c0fd0ccd0401c4d8e
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-17 11:43:34 +0100

    Add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    cleanStr = str.toLowerCase()
+                  .split("")
+                  .filter(str => !str.match(/[^\w]/g))
+                  .join("");
+    reverseStr = cleanStr.split("")
+                    .reverse()
+                    .join("");
+    if (cleanStr === reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
