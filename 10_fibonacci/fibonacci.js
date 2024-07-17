37caaad88a522d14e4e02c64f725c3c83cc8bdee
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-17 12:24:56 +0100

    Add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS"
+    let fibs = [0,1];
+    let i = 2;
+    while (i <= n) {
+        fibs[i] = fibs.slice(i-2).reduce((a, b) => a + b)
+        i++
+    }
+    return fibs[n];
 };
 
 // Do not edit below this line
